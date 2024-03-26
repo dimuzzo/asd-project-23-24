f528f69c8dc7f219dc3439734ae02453d905e130
Alessandro Demo <alessandro.demo@edu.unito.it>
2024-03-26 15:36:20 +0100

    inserimento primi file per ex1

---
+++ ex1/main_ex1.c
@@ -0,0 +1,2 @@
+void merge_sort(void *base, size_t nitems, size_t size, int (*compar)(const void*, const void*));
+void quick_sort(void *base, size_t nitems, size_t size, int (*compar)(const void*, const void*));

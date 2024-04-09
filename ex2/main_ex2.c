47a9bea63d12a347ff5d82c2f897088fd7051bee
Alessandro Demo <alessandro.demo@edu.unito.it>
2024-04-09 16:05:49 +0200

    creazione makefile per esercizio 2

---
+++ ex2/main_ex2.c
@@ -0,0 +1,21 @@
+// libraries declaration
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int edit_distance(const char *s1, const char* s2);
+int edit_distance_dyn(const char *s1, const char* s2);
+
+void cancel(){
+
+}
+
+void insert(){
+    
+}
+
+
+
+int main(){
+    return 0;
+}

0a19dd4ca54d9d1b970fc34a72237b0d5c502605
Alessandro Demo <alessandro.demo@edu.unito.it>
2024-04-23 22:53:56 +0200

    modifica merge sort ex1

--- ex1/main_ex1.c
+++ ex1/main_ex1.c
@@ -23,7 +23,7 @@ void merge_sort(void *base, size_t nitems, size_t size, int (*compar)(const void
 
 void merge_sort_wrap(void *base, size_t size, size_t left, size_t right, int (*compar)(const void*, const void*)){
     if(left < right){
-        size_t mid = (left + right)/2;
+        size_t mid = left + ((right - left)/2);
         merge_sort_wrap(base, size, left, mid, compar);
         merge_sort_wrap(base, size, mid + 1, right, compar);
         merge(base, size, left, mid, right, compar);
@@ -31,28 +31,45 @@ void merge_sort_wrap(void *base, size_t size, size_t left, size_t right, int (*c
 }
 
 void merge(void *base, size_t size, size_t left, size_t mid, size_t right, int (*compar)(const void*, const void*)){
-    int n1 = mid - left + 1;
-    int n2 = right - mid;
-    int arr_1[] = ;
-    int arr_2[] = ;
-    for(int i = 1; i <= n1; i++){
-        arr_1[i] = [left + i - 1];
-    }
-    for(int j = 1; j <= n2; j++){
-        arr_2[j] = [mid + j];
-    }
-    int i = 1;
-    int j = 1;
-    for(int k = left; k <= right; k++){
-        if(arr_1[i] <= arr_2[j]){
-            [k] = arr_1[i];
-            i = i + 1;
+    size_t n1 = mid - left + 1;
+    size_t n2 = right - mid;
+
+    size_t i = 0;
+    size_t j = 0;
+    size_t k = left;
+
+    void arr_1 = malloc(n1 * size);
+    void arr_2 = malloc(n2 * size);
+
+    memcpy(arr_1, (char *)base + left*size, n1*size);
+    memcpy(arr_2, (char *)base + (mid + 1)*size, n2*size);
+
+    while(i < n1 && j < n2){
+        if(compar((char *)arr_1 + i*size, (char *)arr_2 + j*size) <= 0){
+            memcpy((char *)base + k*size, (char *)arr_1 + i*size, size);
+            i++;
         }
         else{
-            [k] = arr_2[j];
-            j = j + 1;
+            memcpy((char *)base + k*size, (char *)arr_2 + j*size, size);
+            j++;
         }
+        k++;
     }
+
+    while(i < n1){
+        memcpy((char *)base + k*size, (char *)arr_1 + i*size, size);
+        i++;
+        k++;
+    }
+
+    while(j < n2){
+        memcpy((char *)base + k*size, (char *)arr_2 + j*size, size);
+        j++;
+        k++;
+    }
+
+    free(arr_1);
+    free(arr_2);
 }
 
 // quick_sort method: algoritmo di ordinamento che fa uso di un elemento centrale (pivot) 
@@ -73,17 +90,22 @@ void quick_sort_wrap(void *base, size_t size, size_t left, size_t right, int (*c
     }
 }
 
-int partition(void *base, size_t size, size_t left, size_t right, int (*compar)(const void*, const void*)){
-    int x = [right];
-    int i = left - 1;
-    for(int j = left; j < right; j++){
-        if( [j] <= x){
-            i = i + 1;
-            [i] = [j];
+size_t partition(void *base, size_t size, size_t left, size_t right, int (*compar)(const void*, const void*)){
+    void *mid = (char *)base + right*size;
+    size_t i = left - 1;
+
+    for(size_t j = left; j < right; j++){
+        if(compar((char *)base + j*size, mid) <= 0){
+            i++;
+            void *temporary = malloc(size);
+            memcpy(temporary, (char *)base + i*size, size);
+            memcpy((char *)base + i*size, (char *)base + j*size, size)
+            memcpy((char *)base + j*size, temporary, size);
+            free(temporary);
+
+            return i+1;
         }
     }
-    [i + 1] = [right];
-    return i + 1;
 }
 
 int main(){

a25b16adb1ea75fd036e1e4f58c3a7e2b7f1d1f8
Alessandro Demo <alessandro.demo@edu.unito.it>
2024-05-28 15:28:51 +0200

    aggiunta nuovi test per edit_distance

--- ex2/src/edit_distance_test.c
+++ ex2/src/edit_distance_test.c
@@ -6,21 +6,48 @@
 #include "unity.h"
 #include "edit_distance.h"
 
-void edit_distance_test(){
+void edit_distance_test_onecanc(){
     TEST_ASSERT(edit_distance_dyn("casa", "cassa") == 1);
+}
+void edit_distance_test_onecanc_oneins(){
     TEST_ASSERT(edit_distance_dyn("casa", "cara") == 2);
+}
+void edit_distance_test_twoins(){
     TEST_ASSERT(edit_distance_dyn("vinaio", "vino") == 2);
+}
+void edit_distance_test_threecanc_oneins(){
     TEST_ASSERT(edit_distance_dyn("tassa", "passato") == 4);
+}
+void edit_distance_test_no_op(){
     TEST_ASSERT(edit_distance_dyn("pioppo", "pioppo") == 0);
-    TEST_ASSERT(edit_distance_dyn("plettro", "spettro") == 2);
+}
+void edit_distance_test_twocanc(){
+    TEST_ASSERT(edit_distance_dyn("petto", "spettro") == 2);
+}
+void edit_distance_test_no_op_void(){
     TEST_ASSERT(edit_distance_dyn("", "") == 0);
+}
+void edit_distance_test_fourcanc_fiveins(){
     TEST_ASSERT(edit_distance_dyn("sette", "anno") == 9);
+}
+void edit_distance_test_null_correctword(){
     TEST_ASSERT(edit_distance_dyn(NULL, "indice") == -1);
+}
+void edit_distance_test_null_wrongword(){
     TEST_ASSERT(edit_distance_dyn("colonia", NULL) == -1);
 }
 
 int main(){
     UNITY_BEGIN();
-    RUN_TEST(edit_distance_test);
+    RUN_TEST(edit_distance_test_onecanc);
+    RUN_TEST(edit_distance_test_onecanc_oneins);
+    RUN_TEST(edit_distance_test_twoins);
+    RUN_TEST(edit_distance_test_threecanc_oneins);
+    RUN_TEST(edit_distance_test_no_op);
+    RUN_TEST(edit_distance_test_twocanc);
+    RUN_TEST(edit_distance_test_no_op_void);
+    RUN_TEST(edit_distance_test_fourcanc_fiveins);
+    RUN_TEST(edit_distance_test_null_correctword);
+    RUN_TEST(edit_distance_test_null_wrongword);
     return UNITY_END();
 }

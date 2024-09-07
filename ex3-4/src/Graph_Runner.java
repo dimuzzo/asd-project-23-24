dc5e7544046bb3db69b020a7b594996fb70ec8c3
Alessandro Demo <alessandro.demo@edu.unito.it>
2024-09-08 01:02:14 +0200

    creazione file per effettuare il running del testing di Graph

---
+++ ex3-4/src/Graph_Runner.java
@@ -0,0 +1,17 @@
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
+public class Graph_Runner{
+    public static void main(String[] args) {
+        Result result = JUnitCore.runClasses(Graph_Testing.class);
+        System.out.println("Total number of tests: " + result.getRunCount());
+        System.out.println("Total number of tests failed: " + result.getFailureCount());
+    
+        for(Failure failure : result.getFailures())
+        {			
+            System.out.println(failure.getMessage());
+        }		
+        System.out.println(result.wasSuccessful());
+    }
+}

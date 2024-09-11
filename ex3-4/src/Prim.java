f5158bd31ade4f58bfba7d2278e58b0f44d2c8b0
Alessandro Demo <alessandro.demo@edu.unito.it>
2024-09-11 22:58:44 +0200

    creazione file per l'algoritmo Prim

---
+++ ex3-4/src/Prim.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.text.*;
+import java.io.*;
+
+public class Prim{
+    public static <V, L extends Number> Collection<? extends AbstractEdge<V, L>> minimumSpanningForest(Graph<V, L> graph) {
+        // calcola la minima foresta ricoprente con l'algoritmo di Prim
+        // restituisce la collezione degli archi che formano la foresta
+    }
+      
+    public static void main(String[] args) {
+        // leggi i dati CSV del grafo dal percorso in args[1]
+        // calcola la minima foresta ricoprente con minimumSpanningForest
+        // scrivi su standard output solo la descrizione della foresta calcolata come CSV con formato analogo a quello in input
+        // su standard error si possono scrivere ulteriori informazioni, come il numero di nodi e archi nella foresta calcolata,
+        // o il peso totale della foresta
+    }  
+}

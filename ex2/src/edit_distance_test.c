a2940a2dfaf893de12e83c5a9d77d2542b531388
Alessandro Demo <alessandro.demo@edu.unito.it>
2024-10-23 13:48:07 +0200

    aggiunti commenti e aggiunta una diversificazione tra i test: statici e dinamici

--- ex2/src/edit_distance_test.c
+++ ex2/src/edit_distance_test.c
@@ -1,4 +1,4 @@
-// libraries declaration
+// Libraries declaration
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -7,39 +7,62 @@
 #include "edit_distance.h"
 #include "unity_internals.h"
 
+// Tests for static edit_distance
 void edit_distance_test_onecanc(){
-    TEST_ASSERT(edit_distance_dyn("casa", "cassa") == 1);
+    TEST_ASSERT(edit_distance("casa", "cassa") == 1);
 }
 void edit_distance_test_onecanc_oneins(){
-    TEST_ASSERT(edit_distance_dyn("casa", "cara") == 2);
+    TEST_ASSERT(edit_distance("casa", "cara") == 2);
 }
 void edit_distance_test_twoins(){
-    TEST_ASSERT(edit_distance_dyn("vinaio", "vino") == 2);
+    TEST_ASSERT(edit_distance("vinaio", "vino") == 2);
 }
 void edit_distance_test_threecanc_oneins(){
-    TEST_ASSERT(edit_distance_dyn("tassa", "passato") == 4);
+    TEST_ASSERT(edit_distance("tassa", "passato") == 4);
 }
 void edit_distance_test_no_op(){
-    TEST_ASSERT(edit_distance_dyn("pioppo", "pioppo") == 0);
+    TEST_ASSERT(edit_distance("pioppo", "pioppo") == 0);
 }
 void edit_distance_test_twocanc(){
-    TEST_ASSERT(edit_distance_dyn("petto", "spettro") == 2);
+    TEST_ASSERT(edit_distance("petto", "spettro") == 2);
 }
 void edit_distance_test_no_op_void(){
-    TEST_ASSERT(edit_distance_dyn("", "") == 0);
+    TEST_ASSERT(edit_distance("", "") == 0);
 }
 void edit_distance_test_fourcanc_fiveins(){
+    TEST_ASSERT(edit_distance("sette", "anno") == 9);
+}
+
+// Tests for dynamic edit_distance
+void edit_distance_test_onecanc_dyn(){
+    TEST_ASSERT(edit_distance_dyn("casa", "cassa") == 1);
+}
+void edit_distance_test_onecanc_oneins_dyn(){
+    TEST_ASSERT(edit_distance_dyn("casa", "cara") == 2);
+}
+void edit_distance_test_twoins_dyn(){
+    TEST_ASSERT(edit_distance_dyn("vinaio", "vino") == 2);
+}
+void edit_distance_test_threecanc_oneins_dyn(){
+    TEST_ASSERT(edit_distance_dyn("tassa", "passato") == 4);
+}
+void edit_distance_test_no_op_dyn(){
+    TEST_ASSERT(edit_distance_dyn("pioppo", "pioppo") == 0);
+}
+void edit_distance_test_twocanc_dyn(){
+    TEST_ASSERT(edit_distance_dyn("petto", "spettro") == 2);
+}
+void edit_distance_test_no_op_void_dyn(){
+    TEST_ASSERT(edit_distance_dyn("", "") == 0);
+}
+void edit_distance_test_fourcanc_fiveins_dyn(){
     TEST_ASSERT(edit_distance_dyn("sette", "anno") == 9);
-}
-void edit_distance_test_null_correctword(){
-    TEST_ASSERT(edit_distance_dyn(NULL, "indice") == -1);
-}
-void edit_distance_test_null_wrongword(){
-    TEST_ASSERT(edit_distance_dyn("colonia", NULL) == -1);
 }
 
 int main(){
     UNITY_BEGIN();
+
+    // Static tests
     RUN_TEST(edit_distance_test_onecanc);
     RUN_TEST(edit_distance_test_onecanc_oneins);
     RUN_TEST(edit_distance_test_twoins);
@@ -48,7 +71,16 @@ int main(){
     RUN_TEST(edit_distance_test_twocanc);
     RUN_TEST(edit_distance_test_no_op_void);
     RUN_TEST(edit_distance_test_fourcanc_fiveins);
-    RUN_TEST(edit_distance_test_null_correctword);
-    RUN_TEST(edit_distance_test_null_wrongword);
+
+    // Dynamic tests
+    RUN_TEST(edit_distance_test_onecanc_dyn);
+    RUN_TEST(edit_distance_test_onecanc_oneins_dyn);
+    RUN_TEST(edit_distance_test_twoins_dyn);
+    RUN_TEST(edit_distance_test_threecanc_oneins_dyn);
+    RUN_TEST(edit_distance_test_no_op_dyn);
+    RUN_TEST(edit_distance_test_twocanc_dyn);
+    RUN_TEST(edit_distance_test_no_op_void_dyn);
+    RUN_TEST(edit_distance_test_fourcanc_fiveins_dyn);
+
     return UNITY_END();
 }

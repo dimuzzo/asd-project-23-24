405ae6fdf4759b0a3c9e5729c897fe249ea33146
Alessandro Demo <alessandro.demo@edu.unito.it>
2024-10-23 13:48:26 +0200

    aggiunti commenti

--- ex2/src/main_test.c
+++ ex2/src/main_test.c
@@ -1,3 +1,4 @@
+// Libraries declaration
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -6,15 +7,16 @@
 #define MAX_WORD_SIZE 30
 
 void main_edit_distance(FILE *correct, FILE *dictionary){
+    char word[MAX_WORD_SIZE]; // Temporary string to read a line from the file
+    char dict[MAX_WORD_SIZE]; // String to read a line from the dictionary
+    int word_readed = 0; // Counter for the number of data read
 
-    char word[MAX_WORD_SIZE]; // Stringa temporanea per leggere una riga dal file
-    char dict[MAX_WORD_SIZE];
-    int word_readed = 0; // Contatore per il numero di dati letti
-
+    // While loop to get the words suggested by the dictionary
+    // Furthermore, there are calls to the tolower() function to also compare words in the file with uppercase letters
     while (fscanf(correct, "%29s", word) == 1) {
         *word = tolower(*word);
-        printf("Parola: \t %s\n", word);
-        printf("Suggerimenti: \n");
+        printf("Word: \t %s\n", word);
+        printf("Suggested words: \n");
         int min_edit = strlen(word);
         while (fscanf(dictionary, "%29s", dict) == 1) {
             *word = tolower(*word);
@@ -40,27 +42,27 @@ int main(int argc, char *argv[]){
         return 1;
     }
 
-    char *filename_read_correct = argv[1]; // Nome file CSV che leggo
-    char *filename_read_dictionary = argv[2]; // Nome file CSV che scrivo
+    char *filename_read_correct = argv[1];
+    char *filename_read_dictionary = argv[2];
 
     FILE *file_input1;
     FILE *file_input2;
    
     file_input1 = fopen(filename_read_correct, "r");
     if (file_input1 == NULL) {
-        printf("Impossibile aprire il file %s\n", filename_read_correct);
+        printf("Unable to open file: %s\n", filename_read_correct);
         return 1;
     }
    
     file_input2 = fopen(filename_read_dictionary, "r");
     if (file_input2 == NULL) {
-        printf("Impossibile aprire il file %s\n", filename_read_dictionary);
+        printf("Unable to open file: %s\n", filename_read_dictionary);
         return 1;
     }
    
     main_edit_distance(file_input1, file_input2);
 
-    // Chiusura dei file
+    // Closing open files
     fclose(file_input1);
     fclose(file_input2);
 

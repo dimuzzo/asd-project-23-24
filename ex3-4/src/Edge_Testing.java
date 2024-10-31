fd2b49b5197b915db0d8df81b4e7255f26b6624d
Alessandro Demo <alessandro.demo@edu.unito.it>
2024-10-31 16:46:44 +0100

    migliorato e ottimizzato il testing

--- ex3-4/src/Edge_Testing.java
+++ ex3-4/src/Edge_Testing.java
@@ -1,31 +1,26 @@
-import java.util.Comparator;
-import java.util.NoSuchElementException;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.assertArrayEquals;
 import org.junit.Before;
 import org.junit.Test;
 
 public class Edge_Testing{
-    private Integer i1, i2, i3;
-    private String s1, s2, s3;
+    private Integer i1, i2;
+    private String s1, s2, l1, l2;
     private Edge<Integer, String> e1;
     private Edge<String, String> e2;
 
     @Before
-    public void createEdge(){
+    public void createNewEdge(){
         i1 = 15;
         i2 = 25;
-        i3 = 40;
         s1 = "elodia";
         s2 = "isola";
-        s3 = "MappaConcetti";
-        e1 = new Edge<Integer, String>(i1, i2, s1);
-        e2 = new Edge<String, String>(s1, s2, s3);
+        l1 = "label1";
+        l2 = "label2";
+        e1 = new Edge<>(i1, i2, l1);
+        e2 = new Edge<>(s1, s2, l2);
     }
 
-    // Test per gli Integer
+    // Test for Integer Edges
     @Test
     public void testGetStart_Int(){
         assertEquals(e1.getStart(), i1);
@@ -38,10 +33,10 @@ public class Edge_Testing{
 
     @Test
     public void testGetLabel_Int(){
-        assertEquals(e1.getLabel(), s1);
+        assertEquals(e1.getLabel(), l1);
     }
 
-    // Test per gli String
+    // Test for String Edges
     @Test
     public void testGetStart_String(){
         assertEquals(e2.getStart(), s1);
@@ -54,7 +49,7 @@ public class Edge_Testing{
 
     @Test
     public void testGetLabel_String(){
-        assertEquals(e2.getLabel(), s3);
+        assertEquals(e2.getLabel(), l2);
     }
 
 }

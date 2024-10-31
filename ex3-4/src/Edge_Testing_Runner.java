523d6c06aa2a0e61cab18641c03cd4ec6d6e6cba
Alessandro Demo <alessandro.demo@edu.unito.it>
2024-10-31 16:48:00 +0100

    ottimizzato runner e aggiunti conferme di fallimento o successo dei test

--- ex3-4/src/Edge_Testing_Runner.java
+++ ex3-4/src/Edge_Testing_Runner.java
@@ -5,13 +5,16 @@ import org.junit.runner.notification.Failure;
 public class Edge_Testing_Runner{
     public static void main(String[] args) {
         Result result = JUnitCore.runClasses(Edge_Testing.class);
+        
         System.out.println("Total number of tests: " + result.getRunCount());
         System.out.println("Total number of tests failed: " + result.getFailureCount());
+        System.out.println("Total number of tests passed: " + (result.getRunCount() - result.getFailureCount()));
+        System.out.println("Total test duration: " + result.getRunTime() + " ms");
     
         for(Failure failure : result.getFailures())
         {			
-            System.out.println(failure.getMessage());
+            System.out.println("The test failed for this reason: " + failure.getMessage());
         }		
-        System.out.println(result.wasSuccessful());
+        System.out.println("The test is successful? " + result.wasSuccessful());
     }
 }

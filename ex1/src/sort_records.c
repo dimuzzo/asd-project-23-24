c73c9eb60fcd12f19338df6bff039f5a1d2e4ab7
Alessandro Demo <alessandro.demo@edu.unito.it>
2024-11-24 19:40:31 +0100

    migliorata la struttura dei record e la gestione del file dei record

--- ex1/src/sort_records.c
+++ ex1/src/sort_records.c
@@ -2,65 +2,116 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "mergesort_quicksort.h"
 #define MAX_LENGTH 1000 // Maximum string length for each field
-#define MAX_NUM_DATA 20000000 // Maximum number of data to read from the file (20 million for local file, 100 for moodle testing)
+#define MAX_NUM_DATA 2000000 // Maximum number of data to read from the file
 
-typedef struct{
+// Record structure
+struct data_rec{
     int id;
-    char f1[500];
-    int f2;
-    double f3;
-}Fields;
+    char field1[1000];  
+    int field2;
+    float field3;
+};
 
-int compar_char(const void *a, const void *b) {
-    return strcmp(((Fields *)a)->f1, ((Fields *)b)->f1);
+// Comparison functions for the different fields of the record
+int compare_data_char(const void *a, const void *b) {
+    struct data_rec *Data_A = (struct data_rec *)a;
+    struct data_rec *Data_B = (struct data_rec *)b;
+    return strcmp(Data_A -> field1, Data_B -> field1);
 }
 
-int compar_int(const void *a, const void *b) {
-    return ((Fields *)a)->f2 - ((Fields *)b)->f2;
-}
-
-int compar_double(const void *a, const void *b) {
-    if (((Fields *)a)->f3 > ((Fields *)b)->f3) return 1;
-    else if (((Fields *)a)->f3 < ((Fields *)b)->f3) return -1;
+int compare_data_int(const void *a, const void *b) {
+    struct data_rec *Data_A = (struct data_rec *)a;
+    struct data_rec *Data_B = (struct data_rec *)b;
+    if ((Data_A -> field2) > (Data_B -> field2)) return 1;
+    else if ((Data_A -> field2) < (Data_B -> field2)) return -1;
     else return 0;
 }
 
-void sort_records(FILE *infile, FILE *outfile, size_t field, size_t algo) {
-    // Record read from the file
-    Fields *records = NULL;
-    size_t count = 0, capacity = 100;
-    field = malloc(capacity * sizeof(Fields));
+int compare_data_float(const void *a, const void *b) {
+    struct data_rec *Data_A = (struct data_rec *)a;
+    struct data_rec *Data_B = (struct data_rec *)b;
+    if ((Data_A -> field3) > (Data_B -> field3)) return 1;
+    else if ((Data_A -> field3) < (Data_B -> field3)) return -1;
+    else return 0;
+}
 
-    while (fscanf(infile, "%d,%49[^,],%d,%f\n", &records[count].id, records[count].f1,
-                  &records[count].f2, &records[count].f3) == 4) {
-        count++;
-        if (count == capacity) {
-            capacity *= 2;
-            records = realloc(records, capacity * sizeof(Fields));
-        }
+// Function to sort records in CSV file
+void sort_records(FILE *infile, FILE *outfile, size_t field, size_t algo) {
+    
+    struct data_rec *records = malloc(MAX_NUM_DATA * sizeof(struct data_rec));
+    char line[MAX_LENGTH];
+    size_t data_count = 0;
+    
+    if(records == NULL){
+        printf("Memory allocation error");
+        return;
+    }
+    
+    // Modify reading to allow strings of arbitrary length
+        while(fgets(line, sizeof(line), infile) != NULL && (data_count < MAX_NUM_DATA)){
+        sscanf(line, "%d,%999[^,],%d,%f\n", &records[data_count].id, records[data_count].field1, &records[data_count].field2, &records[data_count].field3);
+        data_count++;
     }
 
-    int (*compar)(const void*, const void*);
+    // Select the correct comparator
+    int (*compar)(const void *, const void *);
     if (field == 1)
-        compar = compar_char;
+        compar = compare_data_char;
     else if (field == 2)
-        compar = compar_int;
+        compar = compare_data_int;
     else
-        compar = compar_double;
+        compar = compare_data_float;
 
     // Select the sorting algorithm
     if (algo == 1) {
-        merge_sort(records, count, sizeof(Fields), compar);
+        merge_sort(records, data_count, sizeof(struct data_rec), compar);
     } else {
-        quick_sort(records, count, sizeof(Fields), compar);
+        quick_sort(records, data_count, sizeof(struct data_rec), compar);
     }
 
-    // Writes the sorted records into the output file
-    for (size_t i = 0; i < count; i++) {
-        fprintf(outfile, "%d,%s,%d,%f\n", records[i].id, records[i].f1,
-                records[i].f2, records[i].f3);
+    // Write the sorted records to the output file
+    for (size_t i = 0; i < data_count; i++) {
+        fprintf(outfile, "%d,%s,%d,%f\n", records[i].id, records[i].field1, 
+                records[i].field2, records[i].field3);
     }
 
     free(records);
+    return;
+}
+
+int main(int argc, char *argv[]) {
+    
+    if(argc != 5){
+        return 1;
+    }
+    
+    char *set_records = argv[1];
+    char *sorted_records = argv[2];
+    int field = atoi(argv[3]);
+    int algo = atoi(argv[4]);
+    
+    FILE *infile;
+    FILE *outfile;
+
+    infile = fopen(set_records, "r");
+    if (infile == NULL) {
+        printf("Error opening input file.\n");
+        return 1;
+    }
+
+    outfile = fopen(sorted_records, "w");
+    if (outfile == NULL) {
+        printf("Error opening output file.\n");
+        fclose(infile);
+        return 1;
+    }
+
+    sort_records(infile, outfile, field, algo);
+
+    fclose(infile);
+    fclose(outfile);
+
+    return 0;
 }

a598d1ea1ab7e4a17c8fb59198b05b8b85301147
Alessandro Demo <alessandro.demo@edu.unito.it>
2024-11-24 19:38:11 +0100

    modificato file header in maniera corretta e concisa

--- ex2/src/edit_distance.h
+++ ex2/src/edit_distance.h
@@ -1,8 +1,9 @@
-#define MAX_WORD_LENGTH 100
-#define MAX_WORDS 10000
-int load_file(const char *file_path, char words[MAX_WORDS][MAX_WORD_LENGTH]);
-void correct_words(char dictionary[MAX_WORDS][MAX_WORD_LENGTH], int dict_count, char text[MAX_WORDS][MAX_WORD_LENGTH], int text_count);
+#ifndef EDIT_DISTANCE_H
+#define EDIT_DISTANCE_H
+
 int min(int st, int nd, int rd);
 int edit_distance(const char *s1, const char* s2);
 int edit_distance_dyn_recursive(const char *s1, const char *s2, int lens1, int lens2, int **mat);
 int edit_distance_dyn(const char *s1, const char *s2);
+
+#endif

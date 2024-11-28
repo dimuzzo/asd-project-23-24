aac521066a1e7aecadafe750edc99d5a7c95b20f
Alessandro Demo <alessandro.demo@edu.unito.it>
2024-11-28 16:54:18 +0100

    aggiornato file header per la partizione a 3 vie

--- ex1/src/mergesort_quicksort.h
+++ ex1/src/mergesort_quicksort.h
@@ -5,7 +5,7 @@ void merge(void *base, size_t mid, size_t nitems, size_t size, int (*compar)(con
 
 void merge_sort(void *base, size_t nitems, size_t size, int (*compar)(const void *, const void *));
 
-size_t partition(void *base, size_t nitems, size_t size, int (*compar)(const void *, const void *));
+void three_way_partition(void *base, size_t nitems, size_t size, int (*compar)(const void *, const void *), size_t *lt, size_t *gt);
 
 void quick_sort(void *base, size_t nitems, size_t size, int (*compar)(const void *, const void *));
 

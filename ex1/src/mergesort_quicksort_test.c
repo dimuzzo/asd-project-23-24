4f0101c69549fdd15afe4fbd17b9f8217d52f7ef
Alessandro Demo <alessandro.demo@edu.unito.it>
2024-11-28 16:54:49 +0100

    aggiornato file di testing per migliore lettura

--- ex1/src/mergesort_quicksort_test.c
+++ ex1/src/mergesort_quicksort_test.c
@@ -7,7 +7,7 @@
 #include "unity_internals.h"
 
 int compare_data_char(const void *a, const void *b) {
-    return strcmp((char*)a, (char*)b);
+    return (*(char *)a - *(char *)b);
 }
 
 int compare_data_int(const void *a, const void *b) {
@@ -16,7 +16,7 @@ int compare_data_int(const void *a, const void *b) {
     else return 0;
 }
 
-int compare_data_float(const void *a, const void *b) {
+int compare_data_fp(const void *a, const void *b) {
     if (*(float*)a > *(float*)b) return 1;
     else if (*(float*)a < *(float*)b) return -1;
     else return 0;
@@ -44,7 +44,7 @@ void mergesort_char_test_four_elements() {
     TEST_ASSERT_EQUAL_INT8_ARRAY(expected, arr, 4);
 }
 
-// Int
+// Integer
 void mergesort_int_test_zero_elements() {
     int arr[0];
     merge_sort(arr, 0, sizeof(int), compare_data_int);
@@ -64,23 +64,23 @@ void mergesort_int_test_four_elements() {
     TEST_ASSERT_EQUAL_INT_ARRAY(expected, arr, 4);
 }
 
-// Float
-void mergesort_float_test_zero_elements() {
+// Floating Point
+void mergesort_fp_test_zero_elements() {
     float arr[0];
-    merge_sort(arr, 0, sizeof(float), compare_data_float);
+    merge_sort(arr, 0, sizeof(float), compare_data_fp);
     TEST_ASSERT_EQUAL(0, sizeof(arr));
 }
 
-void mergesort_float_test_one_element() {
+void mergesort_fp_test_one_element() {
     float arr[] = {42.42};
-    merge_sort(arr, 1, sizeof(float), compare_data_float);
+    merge_sort(arr, 1, sizeof(float), compare_data_fp);
     TEST_ASSERT_EQUAL_FLOAT(42.42, arr[0]); 
 }
 
-void mergesort_float_test_four_elements() {
+void mergesort_fp_test_four_elements() {
     float arr[] = {4.4, 2.2, 1.1, 3.3};
     float expected[] = {1.1, 2.2, 3.3, 4.4};
-    merge_sort(arr, 4, sizeof(float), compare_data_float);
+    merge_sort(arr, 4, sizeof(float), compare_data_fp);
     TEST_ASSERT_EQUAL_FLOAT_ARRAY(expected, arr, 4);
 }
 
@@ -106,7 +106,7 @@ void quicksort_char_test_four_elements() {
     TEST_ASSERT_EQUAL_INT8_ARRAY(expected, arr, 4);
 }
 
-// Int
+// Integer
 void quicksort_int_test_zero_elements() {
     int arr[0];
     quick_sort(arr, 0, sizeof(int), compare_data_int);
@@ -126,23 +126,23 @@ void quicksort_int_test_four_elements() {
     TEST_ASSERT_EQUAL_INT_ARRAY(expected, arr, 4);
 }
 
-// Float
-void quicksort_float_test_zero_elements() {
+// Floating Point
+void quicksort_fp_test_zero_elements() {
     float arr[0];
-    quick_sort(arr, 0, sizeof(float), compare_data_float);
+    quick_sort(arr, 0, sizeof(float), compare_data_fp);
     TEST_ASSERT_EQUAL(0, sizeof(arr));
 }
 
-void quicksort_float_test_one_element() {
+void quicksort_fp_test_one_element() {
     float arr[] = {42.42};
-    quick_sort(arr, 1, sizeof(float), compare_data_float);
+    quick_sort(arr, 1, sizeof(float), compare_data_fp);
     TEST_ASSERT_EQUAL_FLOAT(42.42, arr[0]); 
 }
 
-void quicksort_float_test_four_elements() {
+void quicksort_fp_test_four_elements() {
     float arr[] = {4.4, 2.2, 1.1, 3.3};
     float expected[] = {1.1, 2.2, 3.3, 4.4};
-    quick_sort(arr, 4, sizeof(float), compare_data_float);
+    quick_sort(arr, 4, sizeof(float), compare_data_fp);
     TEST_ASSERT_EQUAL_FLOAT_ARRAY(expected, arr, 4);
 }
 
@@ -156,15 +156,15 @@ int main(){
     RUN_TEST(mergesort_char_test_one_element);
     RUN_TEST(mergesort_char_test_four_elements);
     
-    // Int tests
+    // Integer tests
     RUN_TEST(mergesort_int_test_zero_elements);
     RUN_TEST(mergesort_int_test_one_element);
     RUN_TEST(mergesort_int_test_four_elements);
     
-    // Float tests
-    RUN_TEST(mergesort_float_test_zero_elements);
-    RUN_TEST(mergesort_float_test_one_element);
-    RUN_TEST(mergesort_float_test_four_elements);
+    // Floating Point tests
+    RUN_TEST(mergesort_fp_test_zero_elements);
+    RUN_TEST(mergesort_fp_test_one_element);
+    RUN_TEST(mergesort_fp_test_four_elements);
 
     // Testing Quick Sort
     
@@ -173,15 +173,15 @@ int main(){
     RUN_TEST(quicksort_char_test_one_element);
     RUN_TEST(quicksort_char_test_four_elements);
     
-    // Int tests
+    // Integer tests
     RUN_TEST(quicksort_int_test_zero_elements);
     RUN_TEST(quicksort_int_test_one_element);
     RUN_TEST(quicksort_int_test_four_elements);
     
-    // Float tests
-    RUN_TEST(quicksort_float_test_zero_elements);
-    RUN_TEST(quicksort_float_test_one_element);
-    RUN_TEST(quicksort_float_test_four_elements);
+    // Floating Point tests
+    RUN_TEST(quicksort_fp_test_zero_elements);
+    RUN_TEST(quicksort_fp_test_one_element);
+    RUN_TEST(quicksort_fp_test_four_elements);
 
     return UNITY_END();
 }

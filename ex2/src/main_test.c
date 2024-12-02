8792e7d3e21adb54c1a9f43bf64f7befccdc3f70
Alessandro Demo <alessandro.demo@edu.unito.it>
2024-12-03 00:53:25 +0100

    migliorata la gestione delle parole con lettere maiuscole, gestito anche meglio il confronto tra distanza minima corrente e distanza minima appena registrata. aggiunti commenti

--- ex2/src/main_test.c
+++ ex2/src/main_test.c
@@ -4,67 +4,84 @@
 #include <string.h>
 #include <ctype.h>
 #include "edit_distance.h"
-#define MAX_WORD_SIZE 30
 
-void main_edit_distance(FILE *correct, FILE *dictionary){
-    char word[MAX_WORD_SIZE]; // Temporary string to read a line from the file
-    char dict[MAX_WORD_SIZE]; // String to read a line from the dictionary
-    int word_readed = 0; // Counter for the number of data read
+#define MAX_WORD_SIZE 50
 
-    // While loop to get the words suggested by the dictionary
-    // Furthermore, there are calls to the tolower() function to also compare words in the file with uppercase letters
-    while (fscanf(correct, "%29s", word) == 1) {
-        *word = tolower(*word);
-        printf("Word: \t %s\n", word);
-        printf("Suggested words: \n");
-        int min_edit = strlen(word);
-        while (fscanf(dictionary, "%29s", dict) == 1) {
-            *word = tolower(*word);
-            int res = edit_distance_dyn(word, dict);
-            if(res < min_edit)
-                min_edit = res;
+void process_edit_distance(FILE *dictionary, FILE *correctme) {
+    char word_to_correct[MAX_WORD_SIZE]; // Word from the "correctme" file
+    char dictionary_word[MAX_WORD_SIZE]; // Word from the dictionary
+    int min_edit_distance;               // Tracks the minimum edit distance for the current word
+
+    // Process each word from the "correctme" file
+    while (fscanf(correctme, "%49s", word_to_correct) == 1) {
+        // Convert the input word to lowercase
+        for (int i = 0; word_to_correct[i]; i++) {
+            word_to_correct[i] = tolower(word_to_correct[i]);
         }
-        rewind(dictionary);
-        while (fscanf(dictionary, "%29s", dict) == 1) {
-            *word = tolower(*word);
-            int res = edit_distance_dyn(word, dict);
-            if(res == min_edit)
-                printf("\t %s\n", dict);
+
+        printf("Word analyzed:\n\t%s\n", word_to_correct);
+        printf("Suggested words:\n");
+
+        min_edit_distance = MAX_WORD_SIZE; // Set initial value for the minimum edit distance
+
+        // First find the minimum edit distance for the current word
+        while (fscanf(dictionary, "%49s", dictionary_word) == 1) {
+            // Convert the dictionary word to lowercase
+            for (int i = 0; dictionary_word[i]; i++) {
+                dictionary_word[i] = tolower(dictionary_word[i]);
+            }
+
+            int edit_distance = edit_distance_dyn(word_to_correct, dictionary_word);
+            if (edit_distance < min_edit_distance) {
+                min_edit_distance = edit_distance;
+            }
         }
-        rewind(dictionary);
-    } 
+
+        rewind(dictionary); // Reset the dictionary file pointer for the printing
+
+        // Then print all dictionary words with the minimum edit distance
+        while (fscanf(dictionary, "%49s", dictionary_word) == 1) {
+            for (int i = 0; dictionary_word[i]; i++) {
+                dictionary_word[i] = tolower(dictionary_word[i]);
+            }
+
+            int edit_distance = edit_distance_dyn(word_to_correct, dictionary_word);
+            if (edit_distance == min_edit_distance) {
+                printf("\t%s\n", dictionary_word);
+            }
+        }
+
+        rewind(dictionary); // Reset the dictionary file pointer for the next word
+    }
 }
 
-int main(int argc, char *argv[]){
-
+int main(int argc, char *argv[]) {
     if (argc != 3) {
-        printf("Usage: %s <file input path correctme.txt> <file input path dictionary.txt> \n", argv[0]);
+        fprintf(stderr, "Usage: %s <dictionary.txt> <correctme.txt>\n", argv[0]);
         return 1;
     }
 
-    char *filename_read_correct = argv[1];
-    char *filename_read_dictionary = argv[2];
+    char *dictionary_path = argv[1];
+    char *correctme_path = argv[2];
 
-    FILE *file_input1;
-    FILE *file_input2;
-   
-    file_input1 = fopen(filename_read_correct, "r");
-    if (file_input1 == NULL) {
-        printf("Unable to open file: %s\n", filename_read_correct);
+    FILE *dictionary_file = fopen(dictionary_path, "r");
+    if (!dictionary_file) {
+        fprintf(stderr, "Error: unable to open the dictionary file '%s'.\n", dictionary_path);
         return 1;
     }
-   
-    file_input2 = fopen(filename_read_dictionary, "r");
-    if (file_input2 == NULL) {
-        printf("Unable to open file: %s\n", filename_read_dictionary);
+
+    FILE *correctme_file = fopen(correctme_path, "r");
+    if (!correctme_file) {
+        fprintf(stderr, "Error: unable to open the file to correct '%s'.\n", correctme_path);
+        fclose(dictionary_file);
         return 1;
     }
-   
-    main_edit_distance(file_input1, file_input2);
 
-    // Closing open files
-    fclose(file_input1);
-    fclose(file_input2);
+    process_edit_distance(dictionary_file, correctme_file);
+
+    // Close open files
+    fclose(dictionary_file);
+    fclose(correctme_file);
 
     return 0;
 }
